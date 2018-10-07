adb86de12da86f5b522f430e2b6a0964f6710099
Cherepahin Artem <cherepahin-artem@ya.ru>
2018-10-08 00:07:09 +0700

    Добавлены классы Item - товар и количество Product - товар Order - документ

---
+++ App/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Item
+    {
+        private Product product;
+        private int q;
+
+        public Item(Product product, int q)
+        {
+            this.product = product;
+            this.q = q;
+        }
+        public Product Product()
+        {
+            return product;
+        }
+        public int Quantity()
+        {
+            return q;
+        }
+    }
+}
---
+++ App/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Product
+    {
+        private readonly string name;
+        private readonly string sku;
+        private int price;
+
+        public Product(string name, string sku, int price)
+        {
+            this.name = name;
+            this.sku = sku;
+            this.price = price;
+        }
+
+        public int Price()
+        {
+            return price; 
+        }
+        public string Name()
+        {
+            return name;
+        }
+        public string SKU()
+        {
+            return sku;
+        }
+    }
+}
